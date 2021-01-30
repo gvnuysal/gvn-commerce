3867d5abe8116b771c92e1c8519b8e5bae7cc93a
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-01-30 11:10:54 +0300

    add auto migration

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,31 +1,34 @@
 using System.Threading.Tasks;
-using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using Core.Interfaces;
+
 namespace API.Controllers
 {
      [ApiController]
      [Route("api/[controller]")]
      public class ProductsController : ControllerBase
      {
-          private readonly StoreContext _context;
-          public ProductsController(StoreContext context)
+          private readonly IProductRepository _repository;
+
+          public ProductsController(IProductRepository repository)
           {
-               _context = context;
+               _repository = repository;              
           }
 
           [HttpGet]
-          public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+          public async Task<ActionResult<List<Product>>> GetProducts()
           {
-               return await _context.Products.ToListAsync();
+                var products=await _repository.GetProductsAsync();
+                
+                return Ok(products);
           }
           [HttpGet]
           [Route("{id}")]
           public async Task<ActionResult<Product>> GetProduct(int id)
           {
-               return  await _context.Products.FindAsync(id);
+                return await _repository.GetProductByIdAsync(id);
           }
      }
 }
