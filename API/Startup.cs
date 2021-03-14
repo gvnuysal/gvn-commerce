bf37dda87a55b20ec04caa60fa37546752738675
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-03-14 09:43:26 +0300

    Add Cors

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,7 +35,7 @@ namespace API
                services.AddControllers();
                services.AddApplicationServices();
                services.AddAutoMapperServices();
-               
+
                services.AddDbContext<StoreContext>(options =>
                {
                     options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
@@ -43,21 +43,31 @@ namespace API
                });
 
                services.AddSwaggerDocumention();
-              
+               
+               services.AddCors(opt =>
+               {
+                    opt.AddPolicy("CorsPolicy", policy =>
+                    {
+                         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    });
+               });
+
           }
 
           // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
           public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
           {
                app.UseMiddleware<ExceptionMiddleware>();
-              
- app.UseSwaggerDocumention();
+
+               app.UseSwaggerDocumention();
                app.UseStatusCodePagesWithReExecute("/errors/{0}");
                app.UseHttpsRedirection();
 
                app.UseRouting();
 
                app.UseStaticFiles();
+
+               app.UseCors("CorsPolicy");
 
                app.UseAuthorization();
 
