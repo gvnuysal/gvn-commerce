87ef4d690c56d98bb07167e31373346f8881427e
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-04-04 17:49:03 +0300

    added shop componet,service

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -42,7 +42,5 @@ namespace Infrastructure.Data
           {
                return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
           }
-
-
      }
 }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'GVN Ecommerce';
+export class AppComponent implements OnInit {
+  title:string="Gvn ECommerce"
+  constructor() {}
+
+  ngOnInit(): void {
+
+    var observe=new Observable<number>(item=>{
+      item.next(1),
+      item.next(2),
+      item.next(5),
+      item.next(7)
+    });
+
+    observe.pipe(
+       filter(x=>x%2==1)
+    ).subscribe(item=>console.log(item))
+
+
+
+  }
 }
