a2980bc6f0a146b19f60b6dc8e14642afba57974
Güven Şeref Uysal <gvnuysal@gmail.com>
2021-08-29 14:01:40 +0300

    Routing eklendi.Lazy load modül çağrımları yapıldı

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'shop',
+    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
+  }, 
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,19 +11,5 @@ export class AppComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-
-    var observe=new Observable<number>(item=>{
-      item.next(1),
-      item.next(2),
-      item.next(5),
-      item.next(7)
-    });
-
-    observe.pipe(
-       filter(x=>x%2==1)
-    ).subscribe(item=>console.log(item))
-
-
-
   }
 }
