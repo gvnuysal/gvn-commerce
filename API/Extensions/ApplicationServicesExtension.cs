ff7de550a602704e34b7999b69c16a6ec21cf465
guven.uysal <guven.uysal@teknosol.com.tr>
2021-10-17 22:29:40 +0300

    redis in kurulması işlemleri

--- API/Extensions/ApplicationServicesExtension.cs
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -4,35 +4,50 @@ using Core.Interfaces;
 using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
-     public static class ApplicationServicesExtension
-     {
-          public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-          {
-               //services.AddScoped<IProductRepository, ProductRepository>();
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration _config)
+        {
+            //services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            //services.AddScoped<IConnectionMultiplexer,ConnectionMultiplexer>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+               services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
+            services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                   {
+                     var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0)
+                                                           .SelectMany(x => x.Value.Errors)
+                                                           .Select(x => x.ErrorMessage).ToArray();
+                     var errorResponse = new ApiValidationErrorResponse
+                     {
+                         Errors = errors
+                     };
 
-               services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-               services.Configure<ApiBehaviorOptions>(options =>
-               {
-                    options.InvalidModelStateResponseFactory = actionContext =>
-                    {
-                         var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0)
-                                                            .SelectMany(x => x.Value.Errors)
-                                                            .Select(x => x.ErrorMessage).ToArray();
-                         var errorResponse = new ApiValidationErrorResponse
-                         {
-                              Errors = errors
-                         };
+                     return new BadRequestObjectResult(errorResponse);
+                 };
 
-                         return new BadRequestObjectResult(errorResponse);
-                    };
+            });
 
-               });
-
-               return services;
-          }
-     }
+            return services;
+        }
+    }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,80 +1,72 @@
-using System.Linq;
-using API.Errors;
 using API.Extensions;
-using API.Helpers;
 using API.Middleware;
-using AutoMapper;
-using Core.Interfaces;
-using Infrastructure;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-
+using StackExchange.Redis;
 
 namespace API
 {
-     public class Startup
-     {
-          private readonly IConfiguration _config;
-          public Startup(IConfiguration config)
-          {
-               _config = config;
-          }
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
 
-          // This method gets called by the runtime. Use this method to add services to the container.
-          public void ConfigureServices(IServiceCollection services)
-          {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
 
-               services.AddControllers();
-               services.AddApplicationServices();
-               services.AddAutoMapperServices();
+            services.AddControllers(); 
+            /* services.AddSingleton<ConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });*/
+            services.AddApplicationServices(_config);
+            services.AddAutoMapperServices();
 
-               services.AddDbContext<StoreContext>(options =>
-               {
-                    options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
-                    options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-               });
+            
+          
+            services.AddSwaggerDocumention();
 
-               services.AddSwaggerDocumention();
-               
-               services.AddCors(opt =>
-               {
-                    opt.AddPolicy("CorsPolicy", policy =>
-                    {
-                         policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
-                    });
-               });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                   {
+                       policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                   });
+            });
 
-          }
+        }
 
-          // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-          public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-          {
-               app.UseMiddleware<ExceptionMiddleware>();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
 
-               app.UseSwaggerDocumention();
-               app.UseStatusCodePagesWithReExecute("/errors/{0}");
-               app.UseHttpsRedirection();
+            app.UseSwaggerDocumention();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+            app.UseHttpsRedirection();
 
-               app.UseRouting();
+            app.UseRouting();
 
-               app.UseStaticFiles();
+            app.UseStaticFiles();
 
-               app.UseCors("CorsPolicy");
+            app.UseCors("CorsPolicy");
 
-               app.UseAuthorization();
+            app.UseAuthorization();
 
-               app.UseEndpoints(endpoints =>
-               {
-                    endpoints.MapControllers();
-               });
-          }
-     }
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
