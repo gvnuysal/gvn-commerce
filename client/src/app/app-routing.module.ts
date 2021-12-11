83500966aeec1fc079487f5b45cca2d52b139ce3
guven.uysal <guven.uysal@teknosol.com.tr>
2021-12-11 22:49:52 +0300

    added chechout component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,19 +3,40 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
-import { HomeComponent } from './home/home.component'; 
+import { HomeComponent } from './home/home.component';
 const routes: Routes = [
-  { path: '', component: HomeComponent,data:{breadcrumb:'Home'} },
-  { path: 'test-error', component: TestErrorComponent,data:{breadcrumb:'Test Errors'} },
-  { path: 'server-error', component: ServerErrorComponent,data:{breadcrumb:'Server Error'} },
-  { path: 'not-found', component: NotFoundComponent,data:{breadcrumb:'Not Found'} },
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  {
+    path: 'test-error',
+    component: TestErrorComponent,
+    data: { breadcrumb: 'Test Errors' },
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+    data: { breadcrumb: 'Server Error' },
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { breadcrumb: 'Not Found' },
+  },
   {
     path: 'shop',
-    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),data:{breadcrumb:'Shop'}
+    loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
+    data: { breadcrumb: 'Shop' },
   },
   {
     path: 'basket',
-    loadChildren: () => import('./basket/basket.module').then((m) => m.BasketModule),data:{breadcrumb:'Basket'}
+    loadChildren: () =>
+      import('./basket/basket.module').then((m) => m.BasketModule),
+    data: { breadcrumb: 'Basket' },
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./checkout/checkout.module').then((m) => m.CheckoutModule),
+    data: { breadcrumb: 'Checkout' },
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IProduct } from 'src/app/shared/models/product'; 
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasketItem } from 'src/app/shared/models/basket';
+import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
@@ -11,21 +13,43 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
-
-  constructor(private shopService: ShopService,private activatedRoute:ActivatedRoute,private bcService:BreadcrumbService) {
-    this.bcService.set('@productDetails','');
+  quantity = 1;
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService,
+    private basketService: BasketService
+  ) {
+    this.bcService.set('@productDetails', '');
   }
 
   ngOnInit(): void {
-    this.loadProduct()
+    this.loadProduct();
   }
 
   loadProduct() {
-    this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe((product) => {
-      this.product = product;
-      this.bcService.set('@productDetails',product.name);
-    },error=>{
-      console.log(error)
-    });
+    this.shopService
+      .getProduct(+this.activatedRoute.snapshot.paramMap.get('id'))
+      .subscribe(
+        (product) => {
+          this.product = product;
+          this.bcService.set('@productDetails', product.name);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  addItemToCart(){
+    this.basketService.addItemToBasket(this.product,this.quantity);
+  }
+  incrementQuantity(){
+    this.quantity++;
+  }
+  decrementQuantity(){
+    this.quantity--;
+    if(this.quantity<0)
+    this.quantity=0;
   }
 }
