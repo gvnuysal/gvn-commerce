fa5a27698ebcfb1147f523dfb8012ae39077ff7e
guven.uysal <guven.uysal@teknosol.com.tr>
2022-01-18 09:02:53 +0300

    added login method and user information after login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators'
+import { filter } from 'rxjs/operators';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 @Component({
   selector: 'app-root',
@@ -8,18 +9,41 @@ import { BasketService } from './basket/basket.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title:string="Gvn ECommerce"
-  constructor(private basketService:BasketService) {}
+  title: string = 'Gvn ECommerce';
+  constructor(
+    private basketService: BasketService,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
-    const basketId=localStorage.getItem('basket_id');
-    if(basketId){
-      this.basketService.getBasket(basketId).subscribe(()=>{
-        console.log("initailized basket")
-      },error=>{
-        console.log(console.error);
-      
-      })
+    this.loadBasket();
+    this.loadCurrentUser()
+  }
+  loadBasket() {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(
+        () => {
+          console.log('initailized basket');
+        },
+        (error) => {
+          console.log(console.error);
+        }
+      );
+    }
+  }
+  loadCurrentUser() {
+    debugger
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(
+        () => {
+          console.log('loaded user');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
   }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
-import { CarouselComponent, CarouselModule, SlideComponent } from 'ngx-bootstrap/carousel';
+import {
+  CarouselComponent,
+  CarouselModule,
+  SlideComponent,
+} from 'ngx-bootstrap/carousel';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent,OrderTotalsComponent],
-  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
-  exports: [PaginationModule, PagingHeaderComponent, PagerComponent,CarouselComponent,SlideComponent,OrderTotalsComponent],
+  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ReactiveFormsModule,
+  ],
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent,
+    PagerComponent,
+    CarouselComponent,
+    SlideComponent,
+    OrderTotalsComponent,
+    ReactiveFormsModule,
+    BsDropdownModule
+  ],
 })
 export class SharedModule {}
