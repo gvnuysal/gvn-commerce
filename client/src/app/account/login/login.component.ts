ccec5c037d17a06a4091257617b8b473adabda27
guven.uysal <guven.uysal@teknosol.com.tr>
2022-01-20 08:22:28 +0300

    login component validation işlemleri

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -6,29 +6,31 @@ import { AccountService } from '../account.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  loginForm:FormGroup
-
-  constructor(private accountService:AccountService,private router:Router) { }
+  constructor(private accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
-    this.createLoginForm()
+    this.createLoginForm();
   }
 
-  createLoginForm(){
-    this.loginForm=new FormGroup({
-      email:new FormControl('',Validators.required),
-      password:new FormControl('',Validators.required)
-    })
+  createLoginForm() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required,Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
+      password: new FormControl('', Validators.required),
+    });
   }
-onSubmit(){
-   this.accountService.login(this.loginForm.value).subscribe(()=>{
-      this.router.navigateByUrl('/shop')
-   },error=>{
-     console.log(error)
-   })
-}
+  onSubmit() {
+    this.accountService.login(this.loginForm.value).subscribe(
+      () => {
+        this.router.navigateByUrl('/shop');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
